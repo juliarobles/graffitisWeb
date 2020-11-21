d0e3d454e46047656eeb42c02f2f6a312b104b29
alejandroat99 <alejandroat99@uma.es>
2020-11-21 21:15:25 +0100

    .gitignores fix

--- graffitiApp/admin.py
+++
@@ -1,6 +0,0 @@
-from django_mongoengine import mongo_admin as admin
-from .models import Publicacion, Usuario, Graffiti
-
-# Register your models here.
-admin.site.register(Publicacion)
-admin.site.register(Usuario)
--- graffitiApp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class GraffitiappConfig(AppConfig):
-    name = 'graffitiApp'
