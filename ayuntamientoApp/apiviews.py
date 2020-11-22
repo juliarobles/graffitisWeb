21166c0e195c6c490a07271bda9fefa864920b1a
DanielRuiz14 <0619466123@uma.es>
2020-11-22 02:41:31 +0100

    Funcionando servicios datos abiertos -Calidad del aire -Carriles bici -Eventos Además se ha importado la librería Unicode

--- ayuntamientoApp/apiviews.py
+++ ayuntamientoApp/apiviews.py
@@ -5,10 +5,13 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.exceptions import ParseError
 from rest_framework.parsers import JSONParser
-import urllib3, json
+import urllib3, json, unidecode
 
 urlCalidadDelAire = 'https://datosabiertos.malaga.eu/recursos/ambiente/calidadaire/calidadaire.json'
 
+def comprobar_distancia(latitud1, latitud2, longitud1, longitud2, rango):
+    return latitud1-latitud2<abs(rango) and longitud1-longitud2<abs(rango) 
+     
 class CalidadDelAireTodo(APIView):
     def get(self, request, pk=None):
         http = urllib3.PoolManager()
@@ -46,19 +49,69 @@ def pnpoly(nvert, vertx, verty, testx, testy):
     c = False
     i = 0
     j = nvert-1
-    print(nvert)
-    print(vertx)
-    print(verty)
-    print(testx)
-    print(testy)
+    # print(nvert)
+    # print(vertx)
+    # print(verty)
+    # print(testx)
+    # print(testy)
     while i < nvert:
         #a = (vertx[j]-vertx[i]) * (testy-verty[i]) / (verty[j]-verty[i]) + vertx[i]
         #print(a) poniendo esto sale división por 0
         #El problema tiene pinta de estar aqui
         if (((verty[i]>testy) != (verty[j]>testy)) and
         (testx < (vertx[j]-vertx[i]) * (testy-verty[i]) / (verty[j]-verty[i]) + vertx[i])):
-            print("hey")
             c = not c
-        i += 1
         j = i
+        i += 1
     return c
+
+class eventos(APIView):
+
+    def get(self, request, contenido,campo=''):
+        http = urllib3.PoolManager()
+        r = http.request('GET',
+        'https://datosabiertos.malaga.eu/api/3/action/datastore_search',
+        fields={'resource_id':'7f96bcbb-020b-449d-9277-1d86bd11b827'}
+        )
+
+        lista = json.loads(r.data)['result']['records']
+        # lista = y['result']['records']
+        resultado = []
+        c= str(contenido)
+        if(campo != ''):
+            for elem in lista:
+                if (unidecode.unidecode(c.casefold()) in unidecode.unidecode(str(elem[campo]).casefold())):
+                    resultado.append(elem)
+        else:
+            for elem in lista:
+                for v in elem.items():
+                    if(unidecode.unidecode(c.casefold()) in unidecode.unidecode(str(v).casefold())):
+                        resultado.append(elem)
+                        break
+        return Response(resultado,status=status.HTTP_200_OK)
+
+class bicis(APIView):
+    
+    def get(self, request, latitud, longitud, rango):
+        http = urllib3.PoolManager()
+        r = http.request(
+            'GET', 
+            'https://datosabiertos.malaga.eu/recursos/transporte/trafico/da_carrilesBici-25830.geojson'
+        )
+        
+        lista = json.loads(r.data)['features']
+        latitud = float(latitud)
+        longitud = float(longitud)
+        rango = float(rango)
+
+        resultado = []
+        for elem in lista:
+            if(elem['geometry']['type']=="Point"):
+                if(comprobar_distancia(elem['geometry']['coordinates'][0], latitud, elem['geometry']['coordinates'][1], longitud, rango)):
+                    resultado.append(elem)
+            else:
+                for punto in elem['geometry']['coordinates']:
+                    if(comprobar_distancia(punto[0], latitud, punto[1], longitud, rango)):
+                        resultado.append(elem)
+                        break
+        return Response(resultado,status=status.HTTP_200_OK)
--- ayuntamientoApp/urls.py
+++ ayuntamientoApp/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url,include
+from django.urls import path
 from rest_framework import routers
 #from ayuntamientoApp.views import views
-from ayuntamientoApp.apiviews import CalidadDelAireTodo, CalidadDelAireCoordenadas
+from ayuntamientoApp.apiviews import eventos, bicis, CalidadDelAireTodo, CalidadDelAireCoordenadas
 
 router = routers.DefaultRouter()
 #router.register(r'calidadDelAire', views.PublicacionViewSet, basename="Calidad del aire" )
@@ -9,4 +10,8 @@ router = routers.DefaultRouter()
 urlpatterns = [
     url(r'^calidadDelAire/$', CalidadDelAireTodo.as_view()),
     url(r'^calidadDelAire/(?P<x>[a-zA-Z0-9-.]+),(?P<y>[a-zA-Z0-9-.]+)$', CalidadDelAireCoordenadas.as_view()),
+    path('eventos/<campo>/<contenido>/', eventos.as_view()),
+    path('eventos/<contenido>/',  eventos.as_view()),
+    path('eventos/',  eventos.as_view()),
+    path('bicis/<latitud>+<longitud>+<rango>/', bicis.as_view())
 ]
