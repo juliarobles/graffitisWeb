d4cb65219e24b1020d364df7fd0d9ad293f5f16e
juliarobles <juliarobles@uma.es>
2020-11-24 23:52:34 +0100

    Añadida definiciones en las consultas de calidad del aire y arregla el problema de los ids, unico problema en los datos del ayuntamiento es que varias urls comparten clases de consulta

--- ayuntamientoApp/apiviews.py
+++ ayuntamientoApp/apiviews.py
@@ -10,6 +10,7 @@ import urllib3, json, unidecode
 from django.http import Http404
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
+from django.utils.decorators import method_decorator
 from threading import Thread
 
 urlCalidadDelAire = 'https://datosabiertos.malaga.eu/recursos/ambiente/calidadaire/calidadaire.json'
@@ -29,23 +30,36 @@ def cargar_url(url):
     return r.data
      
 class CalidadDelAireTodo(APIView):
+    @swagger_auto_schema(operation_description="Devuelve todos los datos respecto a la calidad del aire que ofrece el ayuntamiento de Málaga. Esta consulta es muy ineficiente debido a la inmensa cantidad de datos ofrecidos, por lo que desaconsejamos su uso. En su lugar hay disponible la consulta por paginación.",
+                         responses={200: 'Todo correcto', 404: 'Recurso no encontrado'}) 
     def get(self, request, pk=None):
-        http = urllib3.PoolManager()
-        r = http.request('GET', urlCalidadDelAire)
-        datos = json.loads(r.data)
-        return Response(datos, status=status.HTTP_200_OK)
+        try:
+            http = urllib3.PoolManager()
+            r = http.request('GET', urlCalidadDelAire)
+            datos = json.loads(r.data)
+            return Response(datos, status=status.HTTP_200_OK)
+        except:
+            return Response(status= status.HTTP_404_NOT_FOUND)
 
 #skip va incluido y limit no
 class CalidadDelAirePaginacion(APIView):
+
+    @swagger_auto_schema(operation_description="Devuelve los datos de calidad del aire de forma paginada, en una página de tamaño LIMIT y saltando SKIP datos.",
+                         responses={200: 'Todo correcto', 404: 'Recurso no encontrado'}, operation_id="calidadDelAire_paginacion")
     def get(self, request, limit, skip=0):
         limit = int(self.kwargs.get("limit"))
         try:
             skip = int(self.kwargs.get("skip"))
         except:
             skip = 0
-        http = urllib3.PoolManager()
-        r = http.request('GET', urlCalidadDelAire)
-        datos = json.loads(r.data)
+            # @swagger_auto_schema(operation_description="Devuelve los LIMIT primeros datos de calidad del aire.",
+            #              responses={200: 'Todo correcto', 404: 'Recurso no encontrado'}) 
+        try:
+            http = urllib3.PoolManager()
+            r = http.request('GET', urlCalidadDelAire)
+            datos = json.loads(r.data)
+        except:
+            return Response(status= status.HTTP_404_NOT_FOUND)
         numDatos = len(datos['features'])
         res = []
         if skip+limit > numDatos:
@@ -55,15 +69,21 @@ class CalidadDelAirePaginacion(APIView):
                 res.append(datos['features'][i])
         return Response(res, status=status.HTTP_200_OK)
 
+
 #Esta función asume que cada coordenada esta contenida en una única zona
 #IMPORTANTE que si se pega una coordenada de google maps quitar espacio
 class CalidadDelAireCoordenadas(APIView):
+    @swagger_auto_schema(operation_description="Devuelve los datos de calidad del aire de la zona en la que está contenida la coordenada dada (latitud,longitud). Si esa coordenada no se encuentra devuelve un objeto vacio.",
+                         responses={200: 'Todo correcto', 404: 'Recurso no encontrado'}, operation_id="calidadDelAire_coordenadas") 
     def get(self, request, x, y):
         coordX = float(self.kwargs.get("x"))
-        coordY = float(self.kwargs.get("y"))
-        http = urllib3.PoolManager()
-        r = http.request('GET', urlCalidadDelAire)
-        datos = json.loads(r.data)
+        coordY = float(self.kwargs.get("y"))  
+        try:
+            http = urllib3.PoolManager()
+            r = http.request('GET', urlCalidadDelAire)
+            datos = json.loads(r.data)
+        except:
+            return Response(status= status.HTTP_404_NOT_FOUND)
         numDatos = len(datos['features'])
         zona = {}
         encontrado = False
@@ -102,6 +122,8 @@ def pnpoly(nvert, vertx, verty, testx, testy):
 
 
 class CalidadDelAireDistancia(APIView):
+    @swagger_auto_schema(operation_description="Devuelve los datos de calidad del aire de las zonas cuyo centro estén a igual o menor distancia en kilometros (KM) de las coordenadas dadas (latitud,longitud). Si no se encuentran zonas devuelve un objeto vacio.",
+                         responses={200: 'Todo correcto', 404: 'Recurso no encontrado'}, operation_id="calidadDelAire_distancia") 
     def get(self, request, x, y, km):
         coordX = float(self.kwargs.get("x"))
         coordY = float(self.kwargs.get("y"))
@@ -154,14 +176,14 @@ class EventosID(APIView):
                 return Response(elem, status=200)
         return Response(status=404)
     @swagger_auto_schema(operation_description="Devuelve el evento de id PK. Si no hay devuelve error.",
-                         responses={200: 'Todo correcto', 404:'Elemento no existente'}) 
+                         responses={200: 'Todo correcto', 404:'Elemento no existente'}, operation_id="eventos_id") 
     def get(self, request, pk):
         return self.get_object(request, pk)
 
 # URL: 'https://datosabiertos.malaga.eu/api/3/action/datastore_search'
 class Eventos(APIView):
     @swagger_auto_schema(operation_description="Devuelve los eventos que contengan la subcadena CONTENIDO en la propiedad CAMPO. En el caso de que no se pase ningún campo buscará la cadena en todos los campos del objeto. Si no se le pasa ningún parámetro devolverá todos los eventos. ",
-                         responses={200: 'Todo correcto', 404: 'Campo o contenido no existente'}) 
+                         responses={200: 'Todo correcto', 404: 'Campo o contenido no existente'}, operation_id="eventos_propiedades") 
     def get(self, request, contenido='',campo=''):
 
         lista = json.loads(cargar_url(url_eventos))['result']['records']
@@ -190,8 +212,8 @@ class Eventos(APIView):
             return Response(resultado,status=status.HTTP_200_OK)
 
 class EventosPaginacion(APIView):
-    @swagger_auto_schema(operation_description="Devuelve todos los eventos divididos en páginas de tamaño LIMIT y saltando SKIP páginas.",
-                         responses={200: 'Todo correcto'}) 
+    @swagger_auto_schema(operation_description="Devuelve los eventos de forma paginada, en una página de tamaño LIMIT y saltando SKIP datos.",
+                         responses={200: 'Todo correcto'}, operation_id="eventos_paginacion") 
     def get(self, request, limit, skip=0):
         limit = int(self.kwargs.get("limit"))
         try:
@@ -221,7 +243,7 @@ class Bicis(APIView):
         return Response(status=404)
     
     @swagger_auto_schema(operation_description="Consulta sobre los objetos que esten a una distancia menor de RANGO desde un punto de latitud LATITUD y longitud LONGITUD",
-                         responses={200: 'Todo correcto', 404:'Not found'} )
+                         responses={200: 'Todo correcto', 404:'Not found'}, operation_id="bicis_propiedades")
     def get(self, request, latitud=None, longitud=None, rango=None, pk=None):
         if pk:
              return self.get_object(request, pk)
