19f8af779958ca0ace1eed1a3a93ade2bde9e109
alejandroat99 <alejandroat99@uma.es>
2020-12-06 17:18:15 +0100

    Formulario para añadir una nueva contribución, falta subir la imagen a flickr

--- clienteApp/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class GraffitiForm(forms.Form):
-    imagen = forms.FileField(required=True)
-    estado = forms.CharField(required=True)
-    fechaCaptura = forms.DateField(required=True, label='Fecha de captura', widget=forms.DateInput())
