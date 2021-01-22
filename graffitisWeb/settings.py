04a35091a021ff5b9359d359b662aa37755e3b6b
alejandroat99 <alejandroat99@uma.es>
2021-01-22 23:32:19 +0100

    cambiado directorio de static

--- graffitisWeb/settings.py
+++ graffitisWeb/settings.py
@@ -160,7 +160,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "iweb", "static"),
+    os.path.join(BASE_DIR, "static"),
 )
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
