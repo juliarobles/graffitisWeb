14606901e7debbb27b818859aa8838e479ace438
alejandroat99 <alejandroat99@uma.es>
2021-01-23 21:02:29 +0100

    static

--- graffitisWeb/urls.py
+++ graffitisWeb/urls.py
@@ -57,4 +57,4 @@ urlpatterns = [
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-urlpatterns = format_suffix_patterns(urlpatterns, allowed=['json', 'html','xml'])
+urlpatterns = format_suffix_patterns(urlpatterns, allowed=['json', 'html','xml', 'text/css'])
