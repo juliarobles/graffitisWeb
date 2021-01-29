cbaeb117501c08f171f62d112e83f9b4e561ed34
José Díaz <73610677+Pp-wveDev@users.noreply.github.com>
2021-01-29 03:02:24 +0100

    Queda poco

--- clienteApp/views/tiempoViews.py
+++ clienteApp/views/tiempoViews.py
@@ -33,45 +33,48 @@ def cargar_tiempo(request):
     datos=json.loads(r.data)
     
     vered_hoy = None
-    fecha_hoy = datos['location']['localtime'].split(' ')[0]
+    fecha_hoy = datos['location']['localtime'].split(' ')[0].split('-')
     condicion_hoy = datos['current']['condition']['text']
     temp_hoy = datos['current']['temp_c']
     viento_hoy = datos['current']['wind_kph']
     (cond_hoy, vered_hoy) = parseCondicion(condicion_hoy)
+    f_hoy = fecha_hoy[2]+"-"+fecha_hoy[1]+"-"+fecha_hoy[0]
     
     vered_mañana = None
-    fecha_mañana = datos['forecast']['forecastday'][1]['date']
+    fecha_mañana = datos['forecast']['forecastday'][1]['date'].split('-')
     condicion_mañana = datos['forecast']['forecastday'][1]['day']['condition']['text']
     temp_mañana_max = datos['forecast']['forecastday'][1]['day']['maxtemp_c']
     temp_mañana_min = datos['forecast']['forecastday'][1]['day']['mintemp_c']
     viento_mañana = datos['forecast']['forecastday'][1]['day']['maxwind_kph']
     temp_mañana = str(round(temp_mañana_max)) + '-' + str(round(temp_mañana_min))
     (cond_mañana, vered_mañana) = parseCondicion(condicion_mañana)
+    f_mañana = fecha_mañana[2]+"-"+fecha_mañana[1]+"-"+fecha_mañana[0]
     
     vered_pasado = None
-    fecha_pasado = datos['forecast']['forecastday'][2]['date']
+    fecha_pasado = datos['forecast']['forecastday'][2]['date'].split('-')
     condicion_pasado = datos['forecast']['forecastday'][2]['day']['condition']['text']
     temp_pasado_max = datos['forecast']['forecastday'][2]['day']['maxtemp_c']
     temp_pasado_min = datos['forecast']['forecastday'][2]['day']['mintemp_c']
     viento_pasado = datos['forecast']['forecastday'][2]['day']['maxwind_kph']
     temp_pasado = str(round(temp_pasado_min)) + '-' + str(round(temp_pasado_max))
     (cond_pasado, vered_pasado) = parseCondicion(condicion_pasado)
+    f_pasado = fecha_pasado[2]+"-"+fecha_pasado[1]+"-"+fecha_pasado[0]
     
     # Datos
     data = {
-        'fecha_hoy': fecha_hoy,
+        'fecha_hoy': f_hoy,
         'temp_hoy': temp_hoy,
         'viento_hoy': viento_hoy,
         'cond_hoy': condicion_hoy,
         'vered_hoy': vered_hoy,
         
-        'fecha_tom': fecha_mañana,
+        'fecha_tom': f_mañana,
         'temp_tom': temp_mañana,
         'viento_tom': viento_mañana,
         'cond_tom': condicion_mañana,
         'vered_tom': vered_mañana,
         
-        'fecha_pasado': fecha_pasado,
+        'fecha_pasado': f_pasado,
         'temp_pasado': temp_pasado,
         'viento_pasado': viento_pasado,
         'cond_pasado': condicion_pasado,
