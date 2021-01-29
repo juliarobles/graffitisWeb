d63e735f230879a2b8678fdf9247e90331133482
José Díaz <73610677+Pp-wveDev@users.noreply.github.com>
2021-01-29 01:54:47 +0100

    Seguimos con el tiempo

--- clienteApp/views/tiempoViews.py
+++ clienteApp/views/tiempoViews.py
@@ -20,24 +20,34 @@ def cargar_tiempo(request):
     fecha_hoy = datos['location']['localtime'].split(' ')[0]
     condicion_hoy = datos['current']['condition']['text']
     temp_hoy = datos['current']['temp_c']
+    viento_hoy = datos['current']['maxwind_kph']
     
     fecha_mañana = datos['forecast']['forecastday'][1]['date']
     condicion_mañana = datos['forecast']['forecastday'][1]['day']['condition']['text']
     temp_mañana_max = datos['forecast']['forecastday'][1]['day']['maxtemp_c']
     temp_mañana_min = datos['forecast']['forecastday'][1]['day']['mintemp_c']
+    viento_mañana = datos['forecast']['forecastday'][1]['day']['maxwind_kph']
+    
     temp_mañana = str(round(temp_mañana_max)) + '-' + str(round(temp_mañana_min))
     
     fecha_pasado = datos['forecast']['forecastday'][2]['date']
     condicion_pasado = datos['forecast']['forecastday'][2]['day']['condition']['text']
     temp_pasado_max = datos['forecast']['forecastday'][2]['day']['maxtemp_c']
     temp_pasado_min = datos['forecast']['forecastday'][2]['day']['mintemp_c']
+    viento_pasado = datos['forecast']['forecastday'][2]['day']['maxwind_kph']
     temp_pasado = str(round(temp_pasado_min)) + '-' + str(round(temp_pasado_max))
     
     # Datos
     data = {
         'fecha_hoy': fecha_hoy,
+        'temp_hoy': temp_hoy,
+        'viento_hoy': viento_hoy,
         'fecha_mañana': fecha_mañana,
-        'fecha_pasado': fecha_pasado
+        'temp_mañana': temp_mañana,
+        'viento_mañana': viento_mañana,
+        'fecha_pasado': fecha_pasado,
+        'temp_pasado': temp_pasado,
+        'viento_pasado': viento_pasado,
     }
     
     return render(request, 'tiempo.html', data)
